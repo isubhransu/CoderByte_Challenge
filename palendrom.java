af3e0dd9fb76637d4d6e0ee17223405fbc3dfcee
Subhransu Sekhar Mishra <isubhransu@gmail.com>
2015-01-18 13:42:55 -0700

    updated

--- palendrom.java
+++ palendrom.java
@@ -1,7 +1,10 @@
 /*
 
 
-Have the function PalindromeTwo(str) take the str parameter being passed and return the string true if the parameter is a palindrome, (the string is the same forward as it is backward) otherwise return the string false. The parameter entered may have punctuation and symbols but they should not affect whether the string is in fact a palindrome. For example: "Anne, I vote more cars race Rome-to-Vienna" should return true. 
+Have the function PalindromeTwo(str) take the str parameter being passed and return the string true if the parameter 
+is a palindrome, (the string is the same forward as it is backward) otherwise return the string false. The parameter 
+entered may have punctuation and symbols but they should not affect whether the string is in fact a palindrome. For
+example: "Anne, I vote more cars race Rome-to-Vienna" should return true. 
 */
 import java.util.*; 
 import java.io.*;
