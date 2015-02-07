214c5d69448a6158133035f3f23f496c4be585a6
Subhransu Mishra <subhransumishra@Subhransus-MacBook-Pro.local>
2015-02-06 19:13:49 -0700

    New addition - algorithm section

---
+++ Algorithms/Findlca.java
@@ -0,0 +1,37 @@
+//FIND least common ansester in a binary search tree
+public class Findlca{
+
+     public static void main(String []args){
+     Node root=new Node(20);
+     root.left = new Node(8);
+     root.right = new Node(22);
+     root.left.left = new Node(4);
+     root.left.right = new Node(12);
+     root.left.right.left = new Node(10);
+     root.left.right.right = new Node(14);
+     findlca(root, 10, 14);
+     }
+
+     public static void findlca(Node root, int n1, int n2){
+     while(root!=null){
+         if(root.data>n1 && root.data>n2){
+             root = root.left;
+         }else
+         if(root.data<n1 && root.data<n2){
+             root = root.right;
+         }
+         else{break;}
+     }
+     System.out.println(root.data);
+     }
+}
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+
+    public Node(int dat){
+        this.data = dat;
+    }
+}
